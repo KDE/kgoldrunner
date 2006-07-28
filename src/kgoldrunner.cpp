70806940c29ee7f7130e8371c3a027b982ceed4e
Laurent Montel <montel@kde.org>
2006-07-28 12:29:38 +0000

    Now kde4
    svn path=/trunk/KDE/kdegames/kgoldrunner/; revision=567252

--- src/kgoldrunner.cpp
+++ src/kgoldrunner.cpp
@@ -837,16 +837,7 @@ void KGoldrunner::optionsConfigureKeys()
 
 void KGoldrunner::optionsConfigureToolbars()
 {
-    // use the standard toolbar editor
-#if defined(KDE_MAKE_VERSION)
-# if KDE_VERSION >= KDE_MAKE_VERSION(3,1,0)
     saveMainWindowSettings(KGlobal::config(), autoSaveGroup());
-# else
-    saveMainWindowSettings(KGlobal::config());
-# endif
-#else
-    saveMainWindowSettings(KGlobal::config());
-#endif
 }
 
 void KGoldrunner::newToolbarConfig()
@@ -855,15 +846,7 @@ void KGoldrunner::newToolbarConfig()
     // recreate our GUI, and re-apply the settings (e.g. "text under icons", etc.)
     createGUI();
 
-#if defined(KDE_MAKE_VERSION)
-# if KDE_VERSION >= KDE_MAKE_VERSION(3,1,0)
     applyMainWindowSettings(KGlobal::config(), autoSaveGroup());
-# else
-    applyMainWindowSettings(KGlobal::config());
-# endif
-#else
-    applyMainWindowSettings(KGlobal::config());
-#endif
 }
 
 void KGoldrunner::optionsPreferences()
