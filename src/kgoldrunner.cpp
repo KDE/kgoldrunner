0f51867214fb0b2302913780cb05d0788f871ad7
Pau Garcia i Quiles <pgquiles@elpauer.org>
2007-06-14 12:00:59 +0000

    Fix + operating on char * and char[] and missing )
    svn path=/trunk/KDE/kdegames/kgoldrunner/; revision=675531

--- src/kgoldrunner.cpp
+++ src/kgoldrunner.cpp
@@ -548,8 +548,8 @@ void KGoldrunner::setupActions()
 void KGoldrunner::setupThemes ()
 {
     // Look for themes in files "---/share/apps/kgoldrunner/pics/*.desktop".
-    KGlobal::dirs()->addResourceType ("theme", "data", KCmdLineArgs::aboutData()->appName() +
-                                      "/pics/");
+    KGlobal::dirs()->addResourceType ("theme", "data", QString(KCmdLineArgs::aboutData()->appName()) +
+                                      QString("/pics/"));
 
     QStringList themeFilepaths = KGlobal::dirs()->findAllResources
 	("theme", "*.desktop", KStandardDirs::NoDuplicates); // Find files.
@@ -974,7 +974,7 @@ bool KGoldrunner::getDirectories()
 	KGrMessage::information (this, i18n("Get Folders"),
 	i18n("Cannot find or create user games sub-folder '%1/user/' "
 	"in area '%2' of the KDE user area ($KDEHOME).",
-	 myDir, dirs->resourceDirs ("data").join( ":"));
+	 myDir, dirs->resourceDirs ("data").join( ":")));
 	// result = false;		// Don't abort if user area is missing.
     }
     else {
