a80b05d678e49980a716c4e4b1f59c8a15419ad6
Chusslove Illich <caslav.ilic@gmx.net>
2007-07-02 08:52:55 +0000

    Auto-conversion to new KCmdLineArgs/KAboutData/KComponentData API (trunk/KDE). Watch for the few remaining QString to QByteArray assignments that will not compile without extra specification (typically needs .toLocal8Bit() or .toUtf8()).
    svn path=/trunk/KDE/kdegames/kgoldrunner/; revision=682260

--- src/main.cpp
+++ src/main.cpp
@@ -31,28 +31,28 @@ static const char version[] = "3.0";
 
 int main (int argc, char **argv)
 {
-    KAboutData about("kgoldrunner", I18N_NOOP("KGoldrunner" ),
-    		     version, description,
+    KAboutData about("kgoldrunner", 0, ki18n("KGoldrunner" ),
+    		     version, ki18n(description),
                      KAboutData::License_GPL,
-		     "(C) 2003 Ian Wadham and Marco Krüger");
-    about.addAuthor( "Ian Wadham", I18N_NOOP("Current author"),
+		     ki18n("(C) 2003 Ian Wadham and Marco Krüger"));
+    about.addAuthor( ki18n("Ian Wadham"), ki18n("Current author"),
     		     "ianw2@optusnet.com.au" );
-    about.addAuthor( "Marco Krüger", I18N_NOOP("Original author"),
+    about.addAuthor( ki18n("Marco Krüger"), ki18n("Original author"),
     					"grisuji@gmx.de" );
-    about.addCredit("Mauricio Piacentini",
-		      I18N_NOOP("Port to KDE4, Qt4 and KGameCanvas classes"), 
+    about.addCredit(ki18n("Mauricio Piacentini"),
+		      ki18n("Port to KDE4, Qt4 and KGameCanvas classes"), 
 		      "mauricio@tabuleiro.com");
-    about.addCredit("Maurizio Monge",
-		      I18N_NOOP("KGameCanvas classes for KDE4"), 
+    about.addCredit(ki18n("Maurizio Monge"),
+		      ki18n("KGameCanvas classes for KDE4"), 
 		      "maurizio.monge@gmail.com");
-    about.addCredit("Mauricio Piacentini",
-		      I18N_NOOP("Artwork for runners and default theme"), 
+    about.addCredit(ki18n("Mauricio Piacentini"),
+		      ki18n("Artwork for runners and default theme"), 
 		      "mauricio@tabuleiro.com");
-    about.addCredit("Johann Ollivier Lapeyre",
-		      I18N_NOOP("Artwork for bars and ladders"), 
+    about.addCredit(ki18n("Johann Ollivier Lapeyre"),
+		      ki18n("Artwork for bars and ladders"), 
 		      "johann.ollivierlapeyre@gmail.com");
-    about.addCredit("Eugene Trounev",
-		      I18N_NOOP("Artwork for background of Geek City theme"), 
+    about.addCredit(ki18n("Eugene Trounev"),
+		      ki18n("Artwork for background of Geek City theme"), 
 		      "irs_me@hotmail.com");
 
     KCmdLineArgs::init (argc, argv, &about);
