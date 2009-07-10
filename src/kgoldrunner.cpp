4ce9d7114d45170a65557bbf8a5a8e8c8e2863d3
Burkhard Lück <lueck@hube-lueck.de>
2009-07-10 15:02:35 +0000

    typo fix
    svn path=/trunk/KDE/kdegames/kgoldrunner/; revision=994406

--- src/kgoldrunner.cpp
+++ src/kgoldrunner.cpp
@@ -417,7 +417,7 @@ void KGoldrunner::setupActions()
                                   i18n ("&Hybrid Control (Laptop)"),
                                   i18n ("Pointer controls hero; dig "
                                         "using keyboard."),
-                                  i18n ("Use the the laptop's pointer device "
+                                  i18n ("Use the laptop's pointer device "
                                         "to control the hero's moves, and use "
                                         "the keyboard to dig left and right."));
 
