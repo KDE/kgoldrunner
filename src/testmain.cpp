933714a479ccb4fca94e6668e8e40733559eaab3
Ian Wadham <iandw.au@gmail.com>
2012-06-03 09:38:33 +0000

    Add licences.
    svn path=/branches/work/kgoldrunner-qgv/; revision=1298070

--- src/testmain.cpp
+++ src/testmain.cpp
@@ -1,3 +1,20 @@
+/****************************************************************************
+ *    Copyright 2012  Ian Wadham <iandw.au@gmail.com>                       *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or         *
+ *    modify it under the terms of the GNU General Public License as        *
+ *    published by the Free Software Foundation; either version 2 of        *
+ *    the License, or (at your option) any later version.                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ****************************************************************************/
+
 // This main program is only a vehicle for testing g.h, g.cpp, kgrrenderer.h,
 // kgrrenderer.cpp, kgrthemetypes.h and kgrthemetypes.cpp.  Testmain.cpp, g.h
 // and g.cpp are not part of the final KGoldrunner.
