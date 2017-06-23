d2d3f9b9c61335ad947e04b2862c14fdad8150e3
Andrius Štikonas <andrius@stikonas.eu>
2017-06-23 21:23:19 +0100

    Bump version number to 4.15.

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,7 @@
 static const char description[] =
     I18N_NOOP ("KGoldrunner is a game of action and puzzle solving");
 
-// The intention is to keep the KGoldrunner version number in line with KDE.
-static const char version[] = "4.14";
+static const char version[] = "4.15";
 
 static void addCredits (KAboutData & about);
 
