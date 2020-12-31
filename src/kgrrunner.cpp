93c3fe297a36392b7cae0b11a98a0b93ba7ede80
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-31 20:09:38 +0100

    Convert license statements to SPDX expressions
    Also add/move license files as required by REUSE specification
    
    GIT_SILENT

--- src/kgrrunner.cpp
+++ src/kgrrunner.cpp
@@ -1,27 +1,15 @@
-#include "kgrdebug.h"
+/*
+    SPDX-FileCopyrightText: 2009 Ian Wadham <iandw.au@gmail.com>
 
-/****************************************************************************
- *    Copyright 2009  Ian Wadham <iandw.au@gmail.com>                       *
- *                                                                          *
- *    This program is free software; you can redistribute it and/or         *
- *    modify it under the terms of the GNU General Public License as        *
- *    published by the Free Software Foundation; either version 2 of        *
- *    the License, or (at your option) any later version.                   *
- *                                                                          *
- *    This program is distributed in the hope that it will be useful,       *
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *    GNU General Public License for more details.                          *
- *                                                                          *
- *    You should have received a copy of the GNU General Public License     *
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- ****************************************************************************/
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #include "kgrrunner.h"
 #include "kgrlevelgrid.h"
 #include "kgrrulebook.h"
 #include "kgrlevelplayer.h"
 #include "kgoldrunner_debug.h"
+#include "kgrdebug.h"
 
 KGrRunner::KGrRunner (KGrLevelPlayer * pLevelPlayer, KGrLevelGrid * pGrid,
                       int i, int j, const int pSpriteId,
